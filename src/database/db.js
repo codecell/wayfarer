a411b6d78ed43cd0f3b63334a59b247fe47fc9fc
alfrednoble <alfrednoblex@gmail.com>
2019-07-13 19:03:32 +0100

    update heroku db in db.j

--- src/database/db.js
+++ src/database/db.js
@@ -2,18 +2,17 @@ import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+let pool;
 let database;
 
 if (process.env.NODE_ENV === 'development') {
   database = process.env.DB_DEV;
 } else if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'TEST') {
   database = process.env.DB_TEST;
-} else {
-  database = process.env.DB_PROD;
-}
+} 
 
-const pool = new Pool({
+
+pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database,
@@ -21,8 +20,12 @@ const pool = new Pool({
   port: 5432,
 });
 
+if (process.env.NODE_ENV === 'production') { 
+  pool = new Pool(process.env.DATABASE_URL);
+}
+
 pool.on('connect', () => {
-  console.log( `Connected to ${database} DB!!!`);
+  console.log(`Connected to ${database} DB!!!`);
 });
 
 module.exports = pool;
