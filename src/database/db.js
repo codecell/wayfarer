ebd2837652f3c3dff696ffdb4d51d88577a62d52
alfrednoble <alfrednoblex@gmail.com>
2019-07-13 19:38:01 +0100

    update production settings

--- src/database/db.js
+++ src/database/db.js
@@ -4,24 +4,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 let pool;
 let database;
+if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV === 'development') {
+    database = process.env.DB_DEV;
+  } else if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'TEST') {
+    database = process.env.DB_TEST;
+  } 
 
-if (process.env.NODE_ENV === 'development') {
-  database = process.env.DB_DEV;
-} else if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'TEST') {
-  database = process.env.DB_TEST;
-} 
 
-
-pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database,
-  password: process.env.DB_PASS,
-  port: 5432,
-});
-
-if (process.env.NODE_ENV === 'production') { 
-  pool = new Pool({ database: process.env.DATABASE_URL });
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database,
+    password: process.env.DB_PASS,
+    port: 5432,
+  });
+} else { 
+  pool = new Pool({ connectionString: process.env.DATABASE_URL });
 }
 
 pool.on('connect', () => {
